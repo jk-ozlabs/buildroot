0dee4095acd179d3ae018921b7fb5641b4dac0c2
Jeremy Kerr <jk@ozlabs.org>
2015-04-27 10:31:24 -0500

    package/ipmitool: Update to 1.8.15 and add usb interface patch
    This requires an autoreconf, which requires a NEWS file.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- package/ipmitool/ipmitool.mk
+++ package/ipmitool/ipmitool.mk
@@ -4,9 +4,17 @@
 #
 ################################################################################
 
-IPMITOOL_VERSION = 1.8.14
+IPMITOOL_VERSION = 1.8.15
 IPMITOOL_SITE = http://sourceforge.net/projects/ipmitool/files/ipmitool/$(IPMITOOL_VERSION)/
 IPMITOOL_LICENSE = BSD-4c
 IPMITOOL_LICENSE_FILES = COPYING
+IPMITOOL_AUTORECONF = YES
+IPMITOOL_AUTORECONF_OPT = --install
+
+define IPMITOOL_POST_PATCH_ADD_NEWS
+	touch $(@D)/NEWS
+endef
+
+IPMITOOL_POST_PATCH_HOOKS += IPMITOOL_POST_PATCH_ADD_NEWS
 
 $(eval $(autotools-package))
